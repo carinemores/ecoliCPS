7a4a6a649878ada6f9529526b4bd0d976050f632
Carine Roese Mores <49490676+carinemores@users.noreply.github.com>
2023-11-20 13:41:55 +0100

    Update ont_assembly_flye.sh

--- ont_assembly_flye.sh
+++ ont_assembly_flye.sh
@@ -1,19 +1,6 @@
-#$ -cwd
-#$ -S /bin/bash
-#$ -N flye
-#$ -V
-#$ -pe smp 32
-#$ -l h_vmem=8G
-#$ -e logs/flye.error.log
-#$ -o logs/flye.out.log
-#$ -t 1-24
+#Define variables for the input file and output directory:
+ONT_INPUT="/path/to/ont_reads.fastq"
+OUTPUT_DIR="/path/to/output/directory"
 
-IDS=(echo {01..24})
-ID=barcode${IDS[$SGE_TASK_ID]}
-
-ml Flye/2.9-foss-2018b-Python-2.7.15
-
-mkdir -p scratch/flye
-mkdir -p scratch/flye/$ID
-
-flye --nano-raw scratch/basecall/$ID\_filtered.fastq -o scratch/flye/$ID/ -t 32
+#Run Flye for ONT reads:
+flye --nano-raw $ONT_INPUT -o $OUTPUT_DIR -t <number_of_threads>
